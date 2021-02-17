17262d46321253b1adfc489d5bba9069a3079117
aureuneun <aureuneun@gmail.com>
2021-02-17 14:45:34 +0900

    feat: django paginator (page) ans exception

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10)
-    pages = paginator.get_page(page)
+    try:
+        pages = paginator.page(page)
+    except PageNotAnInteger:
+        return redirect("/")
+    except EmptyPage:
+        return redirect("/")
     return render(
         request,
         "rooms/home.html",
