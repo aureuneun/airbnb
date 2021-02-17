1ccec96ca0afe11e1d9c8b5ed908fe878ab511b4
aureuneun <aureuneun@gmail.com>
2021-02-17 17:21:17 +0900

    feat: class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    try:
-        pages = paginator.page(page)
-    except PageNotAnInteger:
-        return redirect("/")
-    except EmptyPage:
-        return redirect("/")
-    return render(
-        request,
-        "rooms/home.html",
-        {"pages": pages},
-    )
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
