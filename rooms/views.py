39595538b247f140da8d3266ca3d6fc5aa482c3b
aureuneun <aureuneun@gmail.com>
2021-02-18 09:48:51 +0900

    feat: room detail FBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView
+from django.http import Http404
+from django.shortcuts import render
 from . import models
 
 
@@ -8,3 +10,11 @@ class HomeView(ListView):
 
     model = models.Room
     paginate_by = 10
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+    except models.Room.DoesNotExist:
+        raise Http404()
+    return render(request, "rooms/detail.html", {"room": room})
